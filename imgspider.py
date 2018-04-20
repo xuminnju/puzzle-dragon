517642a0b6f36e046cad63470931b6454228753c
xuminnju <290349938@qq.com>
2018-04-20 16:18:17 +0800

    '检测是否有该图片'

---
+++ imgspider.py
@@ -0,0 +1,35 @@
+#-*- coding:utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import os
+
+headers = {
+    "User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+}
+url = 'http://puzzledragonx.com/en/monsterbook.asp'
+html = requests.get(url).text
+soup = BeautifulSoup(html,'lxml')
+numList = soup.select('td.index > div.indexframe > a')
+for i,val in enumerate(numList):
+    numList[i] = re.findall('monster.asp\?n=(.*)', val.get('href'))[0]
+
+
+def get_img(num):
+    try:
+        path = r'D://img/%s.png' % num
+        if os.path.exists(path):
+            print('%s号宠物图片已经下载'%num)
+            return
+        url = 'http://puzzledragonx.com/en/img/monster/MONS_%s.jpg'%num
+        imgbyte = requests.get(uDr//l, headers=headers).content
+        with open(path,'wb') as fn:
+            fn.write(imgbyte)
+            print('%s号宠物图片已下载好'%num)
+    except:
+        print('下载失败')
+
+if __name__ == '__main__':
+    for num in numList:
+        get_img(num)
